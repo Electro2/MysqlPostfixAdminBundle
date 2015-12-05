3a3a4b71c6da52d63b2d64ec3694a7e2ed3d20bf
Electro2 <blaise.barre@gmail.com>
2015-12-05 17:02:10 +0100

    Rename project
    Rename project for update

---
+++ Electro2MysqlPostfixAdminBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Electro2\MysqlPostfixAdminBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class Electro2MysqlPostfixAdminBundle extends Bundle
+{
+}
--- TroisSixMysqlPostfixAdminBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace TroisSix\MysqlPostfixAdminBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class TroisSixMysqlPostfixAdminBundle extends Bundle
-{
-}
